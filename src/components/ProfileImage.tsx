eeea38c826396a0df6b9b5fb33adc351782fe411
Peter Abah <peterabah.ob@gmail.com>
2022-04-24 09:59:37 +0100

    Add ProfileImage component

---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  imgUrl?: string;
+  name: string;
+  className: string;
+};
+
+const ProfileImage = ({ imgUrl, name, className }: Props) => {
+  className += ' w-10 h-10 text-lg grid place-items-center rounded-full bg-gray-700';
+
+  if (imgUrl) {
+    return (
+      <img className={className} src={imgUrl} alt={name} />
+    )
+  }
+  
+  return (
+    <span className={className}>
+      {name[0].toUpperCase()}
+    </span>
+  )
+};
+
+export default ProfileImage;
