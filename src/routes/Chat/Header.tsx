e42d7d8d49d625ed34953e51fe31ee30340c7f7b
Peter Abah <peterabah.ob@gmail.com>
2022-04-29 16:52:14 +0100

    Show profile image in chat route header

--- src/routes/Chat/Header.tsx
+++ src/routes/Chat/Header.tsx
@@ -4,14 +4,21 @@ import { getChatInfo } from '@/lib/chats';
 
 import { MdSearch } from 'react-icons/md';
 import BackBtn from '@/components/BackBtn';
+import ProfileImage from '@/components/ProfileImage';
 
 const Header = ({ chat }: { chat: Chat }) => {
   const { data, error } = useSwr(chat, getChatInfo);
+  const name = data?.name ? data.name : '';
 
   return (
     <header className='flex justify-between p-2'>
       <div className='flex'>
-        <BackBtn className='mr-3' />
+        <BackBtn className='mr-2' />
+        <ProfileImage
+          className='!w-8 !h-8 mr-2'
+          name={name}
+          imgUrl={data?.picture}
+        />
         <h1 className='text-xl font-bold'>{data?.name}</h1>
       </div>
   
