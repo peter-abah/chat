0789d6b2c049d7adff23c7fac62bee7450a88a02
Peter Abah <peterabah.ob@gmail.com>
2022-04-29 14:29:05 +0100

    Add link to next page in new group route

--- src/routes/NewGroup/SelectUsers.tsx
+++ src/routes/NewGroup/SelectUsers.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import useSwr from 'swr';
+import { Link } from 'react-router-dom';
 import { useAppContext } from '@/context/AppContext';
 import { getUsers } from '@/firebase/users';
 import { serializeError } from '@/lib/utils';
 import User from '@/components/User';
 import Loader from '@/components/Loader';
+import {MdArrowForward} from 'react-icons/md';
 
 interface Props {
   participants: string[];
@@ -20,7 +22,7 @@ const SelectUsers = ({participants, onSelectUser}: Props) => {
   users = users.filter(({uid}) => uid !== currentUser?.uid)
   
   return (
-   <>
+   <section>
      {users.map((user) => (
        <User
           key={user.uid}
@@ -29,7 +31,13 @@ const SelectUsers = ({participants, onSelectUser}: Props) => {
           onClick={onSelectUser}
         />
      ))}
-   </>
+     <Link
+        to='form'
+        className='fixed bg-primary text-white bottom-6 right-6 rounded-full p-3'
+      >
+        <MdArrowForward className='text-3xl' />
+      </Link>
+   </section>
   )
 };
 
