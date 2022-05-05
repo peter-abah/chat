0e86d62fa2383859c2df2f9db555930a0b5ff643
Peter Abah <peterabah.ob@gmail.com>
2022-05-05 20:56:50 +0100

    Add leaving and deleting group

--- src/routes/GroupProfile/index.tsx
+++ src/routes/GroupProfile/index.tsx
@@ -1,16 +1,21 @@
 import useSwr from 'swr';
-import { useParams } from 'react-router-dom';
-import { getChat } from '@/firebase/chats';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useAppContext } from '@/context/AppContext';
+
+import { getChat, removeUserFromGroup, deleteGroup } from '@/firebase/chats';
 import { serializeError } from '@/lib/utils';
 
+import { MdExitToApp as MdExit, MdDelete } from 'react-icons/md';
 import Participants from './Participants';
 import BackBtn from '@/components/BackBtn';
 import Loader from '@/components/Loader';
 import ProfileImage from '@/components/ProfileImage';
 
 const GroupProfile = () => {
+  const navigate = useNavigate();
   const { id } = useParams() as { id: string };
   const { data: chat, error } = useSwr(id, getChat);
+  const { currentUser } = useAppContext();
 
   if (error) {
     return (
@@ -20,6 +25,32 @@ const GroupProfile = () => {
 
   if (!chat) return <Loader />;
   if (chat.type !== 'group' ) return null;
+  
+  const leaveGroup = async () => {
+    const shouldLeave = window.confirm('Are you sure you want to leave?')
+    if (!shouldLeave || !currentUser) return;
+
+    try {
+      await removeUserFromGroup(chat, currentUser.uid);
+      navigate('/')
+    } catch (e) {
+      window.alert(serializeError(e))
+    }
+  };
+  
+ const _deleteGroup = async () => {
+    if (currentUser?.uid !== chat.owner) return;
+
+    const shouldDelete = window.confirm('Are you sure you want to delete this group?')
+    if (!shouldDelete) return;
+
+    try {
+      await deleteGroup(chat);
+      navigate('/');
+    } catch (e) {
+      window.alert(serializeError(e))
+    }
+  };
 
   const { name, photoUrl } = chat;
   return (
@@ -37,14 +68,34 @@ const GroupProfile = () => {
         {name}
       </h1>
  
-      <div className='mt-2 px-4 mb-4'>
+      <section className='mt-2 px-4 mb-8'>
         <h2 className='font-bold'>Description</h2>
         <p className='text-sm'>
           Group description which can have many letters is here
         </p>
-      </div>
+      </section>
       
       <Participants chat={chat} />
+      
+      <section className='mt-8 font-bold text-red-600'>
+        <button 
+          onClick={leaveGroup}
+          className='flex items-center px-4 py-3'
+        >
+          <MdExit className='text-2xl ml-2 mr-6' />
+          <span>Leave Group</span>
+        </button>
+        
+        {currentUser?.uid === chat.owner && (
+          <button
+            onClick={_deleteGroup}
+            className='flex items-center px-4 py-3'
+          >
+            <MdDelete className='text-2xl ml-2 mr-6' />
+            <span>Delete Group</span>
+          </button>
+        )}
+      </section>
     </main>
   )
 };
