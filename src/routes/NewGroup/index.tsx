f12851503246c2eb978d4bea2ada16ae3b57613b
Peter Abah <peterabah.ob@gmail.com>
2022-05-06 06:31:31 +0100

    refactor NewGroup route component

--- src/routes/NewGroup/index.tsx
+++ src/routes/NewGroup/index.tsx
@@ -1,53 +1,51 @@
-import React, { useState } from 'react';
-import { Route, Routes, Outlet} from 'react-router-dom';
+import { Route, Routes, Outlet, useNavigate } from 'react-router-dom';
+import useSelectParticipants from '@/hooks/useSelectParticipants';
+import { useAppContext } from '@/context/AppContext';
 import PrivateRoute from '@/routes/PrivateRoute';
 
-import { removeProperty } from '@/lib/utils';
-import SelectUsers from './SelectUsers';
+import Header from '@/components/Header'
+import SelectUsers from '@/components/SelectUsers';
 import GroupForm from './GroupForm';
 
 const NewGroup = () => {
-  const [participants, setParticipants] = useState<{[index: string]: boolean}>({});
+  const { currentUser } = useAppContext();
+  const navigate = useNavigate();
+  const {participants, onSelectUser} = useSelectParticipants();
   
-  const onSelectUser = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const { uid } = e.currentTarget.dataset as { uid: string };
-    if (!uid) return;
-
-    if (participants.hasOwnProperty(uid)) {
-      const filtered = removeProperty(participants, uid);
-      setParticipants(filtered);
-    } else {
-      setParticipants({...participants, [uid]: true})
-    }
-  }
-    
+  const onNext = () => navigate('form');
+  const shouldExclude = (uid: string) => uid === currentUser?.uid;
+  
   return (
-    <>
-      <Outlet />
-
-      <Routes>
+    <Routes>
+      <Route
+        index
+        element={
+          <SelectUsers
+            header={
+              <Header
+                heading='Create group'
+                subheading='Add participants'
+              />
+            }
+            participants={participants}
+            onSelectUser={onSelectUser}
+            onNext={onNext}
+            shouldExclude={shouldExclude}
+          />
+        }
+      />
+      <Route element={
+        <PrivateRoute redirectPath='./' isAuthorized={Object.keys(participants).length > 0} />
+      }>
         <Route
-          index
+          path='form/*' 
           element={
-            <SelectUsers
-              participants={participants}
-              onSelectUser={onSelectUser}
-            />
+           <GroupForm participants={participants} />
           }
         />
-        <Route element={
-          <PrivateRoute redirectPath='./' isAuthorized={Object.keys(participants).length > 0} />
-        }>
-          <Route
-            path='form/*' 
-            element={
-             <GroupForm participants={participants} />
-            }
-          />
-        </Route>
-      </Routes>
-    </>
-  )
+      </Route>
+    </Routes>
+)
 };
 
 export default NewGroup;
