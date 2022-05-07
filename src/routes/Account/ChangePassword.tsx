4a34b6df68a757d4e09337157c89240d19ab0d9b
Peter Abah <peterabah.ob@gmail.com>
2022-05-07 22:04:17 +0100

    Add ChangePassword component

---
+++ src/routes/Account/ChangePassword.tsx
@@ -0,0 +1,112 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+import Header from '@/components/Header';
+import LoadingBar from '@/components/LoadingBar';
+import { updateUserPassword, errorToMsg } from '@/firebase/auth';
+import toast from 'react-hot-toast';
+import { serializeError } from '@/lib/utils';
+
+const formSchema = yup.object({
+  currentPassword: yup.string().required('Current password can not be blank'),
+  newPassword: yup.string().required('Enter password')
+    .min(8, 'Password must have at least 8 characters'),
+  newPasswordConfirmation: yup.string().oneOf(
+    [yup.ref('newPassword'), null],
+    'Passwords must match'
+  )
+}).required();
+
+type FormData = yup.InferType<typeof formSchema>
+
+const ChangePassword = () => {
+  const navigate = useNavigate();
+
+  let { register, handleSubmit, setError, formState } = useForm<FormData>({
+    resolver: yupResolver(formSchema)
+  });
+  const { isSubmitting, errors } = formState;
+
+  const onSubmit = async (d: FormData) => {
+    try {
+      await updateUserPassword(d.currentPassword, d.newPassword);
+
+      navigate(-1);
+      toast.success('Password updated');
+    } catch (e: any) {
+      const [key, message] = Object.entries(errorToMsg(e))[0];
+      if (key === 'password') setError('currentPassword', message);
+
+      toast.error('Unable to change password')
+    }
+  };
+
+  return (
+    <>
+      {isSubmitting && <LoadingBar overlay />}
+      <Header heading='Account' subheading='Change password' />
+
+      <form className='px-4' onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor='current-password'
+            className="font-bold"
+          >
+            Current Password:
+          </label>
+          <input
+            id='current-password'
+            className="px-3 py-2 border rounded-md"
+            type='password'
+            {...register('currentPassword')}
+          />
+          {errors.currentPassword &&
+            <small className="pl-2 text-sm">
+              {errors.currentPassword.message}
+            </small>
+          }
+        </div>
+  
+        <div className="flex flex-col mb-4">
+          <label htmlFor='new-password' className="font-bold">New Password: </label>
+          <input
+            id='new-password' 
+            className="px-3 py-2 border rounded-md"
+            type='password'
+            {...register('newPassword')}
+          />
+          {errors.newPassword && 
+            <small className="pl-2 text-sm">
+              {errors.newPassword.message}
+            </small>
+          }
+        </div>
+        
+        <div className="flex flex-col mb-4">
+          <label htmlFor='new-password-confirmation' className="font-bold">Confirm New Password: </label>
+          <input
+            id='new-password-confirmation'
+            className="px-3 py-2 border rounded-md"
+            type='password'
+            {...register('newPasswordConfirmation')} 
+          />
+          {errors.newPasswordConfirmation && 
+            <small className="pl-2 text-sm">
+              {errors.newPasswordConfirmation.message}
+            </small>
+          }
+        </div>
+
+        <button
+          className="mt-8 flex bg-primary text-white font-bold items-center justify-center px-4 py-2 rounded-lg"
+          type='submit'
+          disabled={isSubmitting}
+        >Save</button>
+      </form>
+  </>
+  )
+};
+
+export default ChangePassword;
--- src/routes/Account/index.tsx
+++ src/routes/Account/index.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Account from './Account';
+import ChangePassword from './ChangePassword';
 
 const Main = () => {
   return (
     <Routes>
       <Route index element={<Account />} />
       <Route path='email' element={<p>Edit Email</p>} />
-      <Route path='password' element={<p>Edit Password</p>} />
+      <Route path='password' element={<ChangePassword />} />
       <Route path='delete' element={<p>Delete account</p>} />
     </Routes>
   );
