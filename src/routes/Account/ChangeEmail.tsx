e3180886c371e3d035041fbe16d5ed4f53981908
Peter Abah <peterabah.ob@gmail.com>
2022-05-07 22:53:53 +0100

    Add ChangeEmail component

---
+++ src/routes/Account/ChangeEmail.tsx
@@ -0,0 +1,106 @@
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+import Header from '@/components/Header';
+import LoadingBar from '@/components/LoadingBar';
+import { updateUserEmail, errorToMsg } from '@/firebase/auth';
+import { auth } from '@/firebase';
+import toast from 'react-hot-toast';
+import { serializeError } from '@/lib/utils';
+
+const formSchema = yup.object({
+  email: yup.string().required('Enter new email.')
+    .email('Enter valid email.'),
+  password: yup.string().required('Enter password')
+}).required();
+
+type FormData = yup.InferType<typeof formSchema>
+
+const ChangeEmail = () => {
+  const navigate = useNavigate();
+
+  let { register, handleSubmit, setError, formState } = useForm<FormData>({
+    resolver: yupResolver(formSchema)
+  });
+  const { isSubmitting, errors } = formState;
+
+  const onSubmit = async ({email, password}: FormData) => {
+    try {
+      await updateUserEmail(email, password);
+
+      navigate(-1);
+      toast.success('Email updated');
+    } catch (e: any) {
+      const [key, message] = Object.entries(errorToMsg(e))[0];
+      if (key === 'password') setError(key, message);
+
+      toast.error('Unable to change email')
+    }
+  };
+
+  return (
+    <>
+      {isSubmitting && <LoadingBar overlay />}
+      <Header heading='Account' subheading='Change email' />
+      
+      <section className='mt-2 px-4 mb-8'>
+        <h2 className='font-bold'>Current Email</h2>
+        <p className='text-sm'>
+          {auth.currentUser?.email || 'No email'}
+        </p>
+      </section>
+
+      <form className='px-4' onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor='email'
+            className="font-bold mb-2"
+          >
+            New Email:
+          </label>
+          <input
+            id='email'
+            className="px-3 py-2 border rounded-md"
+            type='email'
+            {...register('email')}
+          />
+          {errors.email &&
+            <small className="pl-2 text-sm">
+              {errors.email.message}
+            </small>
+          }
+        </div>
+  
+        <div className="flex flex-col mb-4">
+          <label
+            htmlFor='password'
+            className="font-bold mb-2"
+          >
+            Password:
+          </label>
+          <input
+            id='password' 
+            className="px-3 py-2 border rounded-md"
+            type='password'
+            {...register('password')}
+          />
+          {errors.password && 
+            <small className="pl-2 text-sm">
+              {errors.password.message}
+            </small>
+          }
+        </div>
+
+        <button
+          className="mt-8 flex bg-primary text-white font-bold items-center justify-center px-4 py-2 rounded-lg"
+          type='submit'
+          disabled={isSubmitting}
+        >Save</button>
+      </form>
+  </>
+  )
+};
+
+export default ChangeEmail;
--- src/routes/Account/index.tsx
+++ src/routes/Account/index.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Account from './Account';
 import ChangePassword from './ChangePassword';
+import ChangeEmail from './ChangeEmail';
 
 const Main = () => {
   return (
     <Routes>
       <Route index element={<Account />} />
-      <Route path='email' element={<p>Edit Email</p>} />
+      <Route path='email' element={<ChangeEmail />} />
       <Route path='password' element={<ChangePassword />} />
       <Route path='delete' element={<p>Delete account</p>} />
     </Routes>
